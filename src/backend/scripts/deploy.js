2b81792af20664133ddbf16a82cda82d6df9d446
Alexis Davidson <alexisdavidson00@gmail.com>
2023-02-26 17:08:29 +0700

    deploy goerli

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -10,32 +10,32 @@ async function main() {
   const NFT = await ethers.getContractFactory("NFT");
 
   //Goerli
-  // const ownerWallet = "0x944932B3551e6302c0e0b1291064d66ACA205f24"
-  // const whitelistRoot = "0x900f6d55f0c0a25f87a71bdfa24f79de172760bb9d48fdb71d762b9c0f526a7e"
-  // const freeAirDropAddresses = ["0x944932B3551e6302c0e0b1291064d66ACA205f24"]
-  // const freeAirDropAddressesAmount = [24]
+  const ownerWallet = "0x944932B3551e6302c0e0b1291064d66ACA205f24"
+  const whitelistRoot = "0x900f6d55f0c0a25f87a71bdfa24f79de172760bb9d48fdb71d762b9c0f526a7e"
+  const freeAirDropAddresses = ["0x944932B3551e6302c0e0b1291064d66ACA205f24"]
+  const freeAirDropAddressesAmount = [24]
 
   //Mainnet
-  const ownerWallet = "0xc4728ce1fB0082BE064ec71fd86E0238C454E858"
-  const whitelistRoot = "0x900f6d55f0c0a25f87a71bdfa24f79de172760bb9d48fdb71d762b9c0f526a7e"
-  const freeAirDropAddresses = [
-    "0x91124b4446e99CaecC01E12c7c261d088571fE08",
-    "0xf854441935334F778502f89B96D60aa1B63faa14",
-    "0x91bec01563B98023B493020F7A1c34A2C2eA305E",
-    "0xAd02CfF24091D2E39026a06E4e7200a9C0183C5C",
-    "0x269b7Fb9F7Be8945E6d0fD5c132E86c79ab55D2B",
-    "0xfdC87078E0d38C1fF22aF81B7294df2Bca441925",
-    "0x944932B3551e6302c0e0b1291064d66ACA205f24",
-  ]
-  const freeAirDropAddressesAmount = [
-    1,
-    2,
-    1,
-    1,
-    1,
-    1,
-    24,
-  ]
+  // const ownerWallet = "0xc4728ce1fB0082BE064ec71fd86E0238C454E858"
+  // const whitelistRoot = "0x900f6d55f0c0a25f87a71bdfa24f79de172760bb9d48fdb71d762b9c0f526a7e"
+  // const freeAirDropAddresses = [
+  //   "0x91124b4446e99CaecC01E12c7c261d088571fE08",
+  //   "0xf854441935334F778502f89B96D60aa1B63faa14",
+  //   "0x91bec01563B98023B493020F7A1c34A2C2eA305E",
+  //   "0xAd02CfF24091D2E39026a06E4e7200a9C0183C5C",
+  //   "0x269b7Fb9F7Be8945E6d0fD5c132E86c79ab55D2B",
+  //   "0xfdC87078E0d38C1fF22aF81B7294df2Bca441925",
+  //   "0x944932B3551e6302c0e0b1291064d66ACA205f24",
+  // ]
+  // const freeAirDropAddressesAmount = [
+  //   1,
+  //   2,
+  //   1,
+  //   1,
+  //   1,
+  //   1,
+  //   24,
+  // ]
 
   const nft = await NFT.deploy(ownerWallet, whitelistRoot, freeAirDropAddresses, freeAirDropAddressesAmount);
   console.log("NFT contract address", nft.address)
