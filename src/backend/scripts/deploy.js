d2c053e7ade9f653a7f29d9f48eef99aac88f8b3
Alexis Davidson <alexisdavidson00@gmail.com>
2023-04-22 16:20:00 +0700

    Update allowlist

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -11,7 +11,7 @@ async function main() {
 
   //Goerli
   const ownerWallet = "0xc4728ce1fB0082BE064ec71fd86E0238C454E858"
-  const allowListRoot = "0x5e135ed4f1ce6c413acae977ea28fc514d89423f919b9f771ec6000bc1a2a898"
+  const allowListRoot = "0xec2a9c923185fc4865da2bd18b19388b2d77f71527b171f454b4596b0f86b2d8"
   const primeListRoot = "0x562e5699f570a2be8a3aead89eae52d403994b0fb2006cca381952c18d4df318"
 
   //Mainnet
