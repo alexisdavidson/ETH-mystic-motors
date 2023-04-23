17f6144f809d89676159e5966c44425ddc095461
Alexis Davidson <alexisdavidson00@gmail.com>
2023-04-23 12:29:39 +0700

    update deploy script

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -10,27 +10,27 @@ async function main() {
   const NFT = await ethers.getContractFactory("NFT");
 
   //Goerli
-  const ownerWallet = "0xc4728ce1fB0082BE064ec71fd86E0238C454E858"
-  const allowListRoot = "0xa43eb350b9bc5997aaaf55ab767e40e06826a2911293275e22e0ade18f02ab1e"
-  const primeListRoot = "0x562e5699f570a2be8a3aead89eae52d403994b0fb2006cca381952c18d4df318"
+  // const ownerWallet = "0xc4728ce1fB0082BE064ec71fd86E0238C454E858"
+  // const allowListRoot = "0xa43eb350b9bc5997aaaf55ab767e40e06826a2911293275e22e0ade18f02ab1e"
+  // const primeListRoot = "0x562e5699f570a2be8a3aead89eae52d403994b0fb2006cca381952c18d4df318"
 
   //Mainnet
-  // const ownerWallet = "0xc4728ce1fB0082BE064ec71fd86E0238C454E858"
-  // const allowListRoot = "0xf30a51f9ebda7cede979db22b84570d8d37b5c97ca9c9a464196d6b2ca5c8c77"
-  // const primeListRoot = "0x17774ea8852ddfe8b6cf569e4317904b16f7931e56659161b102be2270a21ec8"
+  const ownerWallet = "0xc4728ce1fB0082BE064ec71fd86E0238C454E858"
+  const allowListRoot = "0xf30a51f9ebda7cede979db22b84570d8d37b5c97ca9c9a464196d6b2ca5c8c77"
+  const primeListRoot = "0x17774ea8852ddfe8b6cf569e4317904b16f7931e56659161b102be2270a21ec8"
   
-  const nft = await NFT.deploy(deployer.address, allowListRoot, primeListRoot); // mainnet replace
+  const nft = await NFT.deploy(ownerWallet, allowListRoot, primeListRoot);
   console.log("NFT contract address", nft.address)
   saveFrontendFiles(nft, "NFT");
   
   // GOERLI TEST FUNCTIONS TO COMMENT FOR MAINNET
-  await nft.setMintEnabled(true);
-  await nft.setPublicSaleEnabled(true);
-  await nft.mint(1, [], {value: toWei(0.025)})
-  await nft.setMintEnabled(false);
-  await nft.setPublicSaleEnabled(false);
-  await nft.transferOwnership(ownerWallet);
-  console.log("Test functions called", parseInt(await nft.balanceOf(deployer.address)))
+  // await nft.setMintEnabled(true);
+  // await nft.setPublicSaleEnabled(true);
+  // await nft.mint(1, [], {value: toWei(0.025)})
+  // await nft.setMintEnabled(false);
+  // await nft.setPublicSaleEnabled(false);
+  // await nft.transferOwnership(ownerWallet);
+  // console.log("Test functions called", parseInt(await nft.balanceOf(deployer.address)))
 }
 
 function saveFrontendFiles(contract, name) {
